be0726dd19e57054445e2b365ff1a0ceac559510
AGUSTIN MERINO <ingeniero.agustin@gmail.com>
2020-05-13 18:01:35 -0500

    add date and time system

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -17,6 +17,10 @@ def call(Map config=[:]){
                
             }
     }
+    def date = new Date()
+    def sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss")
+    println "Date and Time Is: ${sdf.format(date)}"
+    
     if(config.changes != "false"){
         println "changes"
     }
