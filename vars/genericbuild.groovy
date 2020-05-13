43309b0ca076ef2f45104951016e630952580438
AGUSTIN MERINO <ingeniero.agustin@gmail.com>
2020-05-13 18:48:41 -0500

    test generic

---
+++ vars/genericbuild.groovy
@@ -0,0 +1,26 @@
+def call(Map config=[:]){
+    node{
+        stage('CMS'){
+            echo 'Gathering code from version control...'
+            git branch:'${branch}' , url: 'https://github.com/FeynmanFan/JenkinsGroovy.git'
+        }
+        stage('Build'){
+            try{
+                echo 'Building ...'
+                releasenotes(changes:"true")
+            }catch(ex){
+                echo 'something when wrong'
+                echo ex.toString()
+                current.Build.result = 'FAILURE'
+            }
+
+            
+        }
+        stage('Test'){
+            echo 'Testing ...'
+        }
+        stage('Deploy'){
+            echo 'Deploying ...'
+        }
+    }
+}
