5d6331d81cb5bd83384f888c997c5184713cb673
AGUSTIN MERINO <ingeniero.agustin@gmail.com>
2020-05-13 18:18:07 -0500

    add info commit

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -1,6 +1,7 @@
 import java.io.*;
 import groovy.io.*;
 import java.text.*
+import hudson.model.*;
 
 @NonCPS
 def call(Map config=[:]){
@@ -22,6 +23,18 @@ def call(Map config=[:]){
     def sdf  = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss")
     println "Date and Time Is: ${sdf.format(date)}"
     println "Build Number is: ${BUILD_NUMBER}"
+
+    def changeLogSets = currentBuild.changeSets;
+    for(change in changeLogSets){
+        def entries = change.items;
+        for(entry in entries){
+            echo "${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg} "
+            for(file in entry.affectedFiles){
+                echo "  ${file.editType.name} ${file.path}"
+            }
+
+        }
+    }
     if(config.changes != "false"){
         println "changes"
     }
