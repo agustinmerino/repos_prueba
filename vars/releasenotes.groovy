108e10bf5f650d0b2758aadea977f2b973a0e0d9
AGUSTIN MERINO <ingeniero.agustin@gmail.com>
2020-05-13 18:22:45 -0500

    include inside file block

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -18,24 +18,25 @@ def call(Map config=[:]){
                     }
                
             }
-    }
-    def date = new Date()
-    def sdf  = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss")
-    println "Date and Time Is: ${sdf.format(date)}"
-    println "Build Number is: ${BUILD_NUMBER}"
+    
+        def date = new Date()
+        def sdf  = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss")
+        println "Date and Time Is: ${sdf.format(date)}"
+        println "Build Number is: ${BUILD_NUMBER}"
 
-    def changeLogSets = currentBuild.changeSets;
-    for(change in changeLogSets){
-        def entries = change.items;
-        for(entry in entries){
-            echo "${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg} "
-            for(file in entry.affectedFiles){
-                echo "  ${file.editType.name} ${file.path}"
+        def changeLogSets = currentBuild.changeSets;
+        for(change in changeLogSets){
+            def entries = change.items;
+            for(entry in entries){
+                echo "${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg} "
+                for(file in entry.affectedFiles){
+                    echo "  ${file.editType.name} ${file.path}"
+                }
+
             }
-
         }
-    }
-    if(config.changes != "false"){
-        println "changes"
+        if(config.changes != "false"){
+            println "changes"
+        }
     }
 }
